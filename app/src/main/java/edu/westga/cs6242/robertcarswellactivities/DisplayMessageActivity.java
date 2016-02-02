05b27e5eae86aba0ebcf6a1f96ee59b1f69cfbc3
Robert Carswell <rcarswel@westga.edu>
2016-02-02 16:39:21 -0500

    Display the Message

--- app/src/main/java/edu/westga/cs6242/robertcarswellactivities/DisplayMessageActivity.java
+++ app/src/main/java/edu/westga/cs6242/robertcarswellactivities/DisplayMessageActivity.java
@@ -7,11 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
-
-    Intent intent = getIntent();
-    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +24,22 @@ public class DisplayMessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null)
+                        .show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 }
